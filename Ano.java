a7b1e291d20e961036f9200a480c5abaaeff1be7
João Manoel Freitas Rolim <joao.mfreitas76@gmail.com>
2014-10-21 19:28:45 -0300

    Classe para testar ano

---
+++ Ano.java
@@ -0,0 +1,12 @@
+class Ano{
+	public static void main(String[] args) {
+		int idade= 18;
+
+		System.out.println(idade);
+
+		int anoProximo;
+		anoProximo = idade+1;
+
+		System.out.println(anoProximo);
+	}
+}
